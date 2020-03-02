0b50c846ed0e18da80bf85c0c1536c0e9e9fb329
Laila Campbell <lailacampbell96@gmail.com>
2020-03-02 00:33:19 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,4 +1,32 @@
 class Song 
   attr_accessor name:, artist:, genre
   @@count=0 
-  
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@count += 1
+    @@artists << artist
+    @@genres << genre
+  end
+
+  def self.count
+    @@count
+  end
+
+  def self.artists
+    @@artists.uniq
+  end
+
+  def self.genres
+    @@genres.uniq
+  end
+
+  def self.genre_count
+    @@genres.inject(Hash.new(0)) { |memo, genre| memo[genre] += 1; memo }
+  end
+
+  def self.artist_count
+    @@artists.inject(Hash.new(0)) { |memo, artist| memo[artist] += 1; memo }
+  end
+end
